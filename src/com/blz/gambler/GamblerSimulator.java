5e741829c49222eb2590c6fff908d6813faf0b6d
TejShind <tej.gholap25@gmail.com>
2022-02-24 08:26:22 +0530

    [Tejaswini]ADDED:Feature for Welcome to the Gamber Simulator program.

---
+++ src/com/blz/gambler/GamblerSimulator.java
@@ -0,0 +1,8 @@
+package com.blz.gambler;
+
+public class GamblerSimulator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Gambler Simulator!");
+    }
+}
