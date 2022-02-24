72b1065e1c2dad7658b6ff4c11b1b93cad22f49f
TejShind <tej.gholap25@gmail.com>
2022-02-24 09:27:14 +0530

    [Tejaswini]ADDED:Feature for Check Win or Loose

--- src/com/blz/gambler/GamblerSimulator.java
+++ src/com/blz/gambler/GamblerSimulator.java
@@ -2,11 +2,17 @@ package com.blz.gambler;
 
 public class GamblerSimulator {
     public static final int DAY_STAKE = 100;
-    public static final int Bet = 1;
+    public static final int BET = 1;
 
     public static void main(String[] args) {
+        int totalCsh = DAY_STAKE;
+        System.out.println("Initial Stake is " + DAY_STAKE + "$  and Bet is " + BET + "$ ");
 
-        System.out.println("Initial Stake is " + DAY_STAKE + " and Bet is " + Bet );
-
+        int bet = (int) (Math.floor(Math.random() * 10) % 2);
+        if (bet == 1) {
+            System.out.println("WON $1");
+        } else {
+            System.out.println("LOOSE $1");
+        }
     }
 }
