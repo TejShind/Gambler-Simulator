c078f96b06174173608a6c894889cf6ebdd66d9d
TejShind <tej.gholap25@gmail.com>
2022-02-24 22:22:49 +0530

    [Tejaswini]ADDED:Feature for Gambler Won or Lost 50% stack and resign for the day

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Gambler-Simulator
 1) Welcome message.
-2) Initialize Stake and Bet contatnts.
-3)Checkin Gambler Won $1 and Loose $1 .
+2) Initialize Stake and Bet constants.
+3) Checking Gambler Won $1 and Loose $1 .
+4) Check Gambler Won or Lost 50% stack and resign for the day.
--- src/com/blz/gambler/GamblerSimulator.java
+++ src/com/blz/gambler/GamblerSimulator.java
@@ -3,16 +3,31 @@ package com.blz.gambler;
 public class GamblerSimulator {
     public static final int DAY_STAKE = 100;
     public static final int BET = 1;
+    public static final int PERCENTAGE_50 = 50; //(DAY_STAKE / 100) * 50;
+    public static final int Winning_resign = DAY_STAKE + PERCENTAGE_50;
+    public static final int Losing_resign = DAY_STAKE - PERCENTAGE_50;
 
     public static void main(String[] args) {
-        int totalCsh = DAY_STAKE;
+        int totalCash = DAY_STAKE;
         System.out.println("Initial Stake is " + DAY_STAKE + "$  and Bet is " + BET + "$ ");
-
-        int bet = (int) (Math.floor(Math.random() * 10) % 2);
-        if (bet == 1) {
-            System.out.println("WON $1");
+        while (totalCash < Winning_resign && totalCash > Losing_resign) {
+            int bet = (int) (Math.floor(Math.random() * 10) % 2);
+            System.out.println(bet);
+            if (bet == 1) {
+                totalCash += BET;
+                System.out.println("Gambler WON the bet  " + totalCash);
+            } else {
+                totalCash -= BET;
+                System.out.println("Gambler LOST the bet " + totalCash);
+            }
+        }
+        if (totalCash == Winning_resign) {
+            System.out.println("Gambler WON by " + totalCash);
         } else {
-            System.out.println("LOOSE $1");
+            System.out.println("Gambler LOST by " + totalCash);
+
         }
     }
 }
+
+
