f1d823a187f7b0f0dfef0e790b5286be4696a4f0
TejShind <tej.gholap25@gmail.com>
2022-02-25 08:08:50 +0530

    [Tejaswini]ADDED:Feature for Gambler Won and Lost stake within 20 Days

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2) Initialize Stake and Bet constants.
 3) Checking Gambler Won $1 and Loose $1 .
 4) Check Gambler Won or Lost 50% stack and resign for the day.
+5) Gambler Won and Lost stake within 20 Days.
--- src/com/blz/gambler/GamblerSimulator.java
+++ src/com/blz/gambler/GamblerSimulator.java
@@ -6,28 +6,43 @@ public class GamblerSimulator {
     public static final int PERCENTAGE_50 = 50; //(DAY_STAKE / 100) * 50;
     public static final int Winning_resign = DAY_STAKE + PERCENTAGE_50;
     public static final int Losing_resign = DAY_STAKE - PERCENTAGE_50;
+    public static final int START_DAY = 1;
+    public static final int MAXIMUM_DAY = 20;
+    public static final int STAKES_PER_DAY = 50;
+    public static final int START_DOLLARS = 0;
 
     public static void main(String[] args) {
+        int days = START_DAY;
         int totalCash = DAY_STAKE;
+        int totalDollars = START_DOLLARS;
+
         System.out.println("Initial Stake is " + DAY_STAKE + "$  and Bet is " + BET + "$ ");
-        while (totalCash < Winning_resign && totalCash > Losing_resign) {
-            int bet = (int) (Math.floor(Math.random() * 10) % 2);
-            System.out.println(bet);
-            if (bet == 1) {
-                totalCash += BET;
-                System.out.println("Gambler WON the bet  " + totalCash);
+        for (days = START_DAY; days <= MAXIMUM_DAY; days++) {
+
+            while (totalCash < Winning_resign && totalCash > Losing_resign) {
+                int bet = (int) (Math.floor(Math.random() * 10) % 2);
+                System.out.println(bet);
+                if (bet == 1) {
+                    totalCash += BET;
+                    System.out.println("Gambler WON the bet  " + totalCash);
+                } else {
+                    totalCash -= BET;
+                    System.out.println("Gambler LOST the bet " + totalCash);
+                }
+            }
+            if (totalCash == Winning_resign) {
+                totalDollars += STAKES_PER_DAY;
+                System.out.println("Gambler WON by " + totalCash);
             } else {
-                totalCash -= BET;
-                System.out.println("Gambler LOST the bet " + totalCash);
+                totalDollars -= STAKES_PER_DAY;
+                System.out.println("Gambler LOST by " + totalCash);
             }
         }
-        if (totalCash == Winning_resign) {
-            System.out.println("Gambler WON by " + totalCash);
+        if (totalDollars > START_DOLLARS) {
+            System.out.println("Gambler is won the " + totalDollars);
         } else {
-            System.out.println("Gambler LOST by " + totalCash);
-
+            System.out.println("Gambler is lost the " + totalDollars);
         }
     }
 }
-
 
